61f121d8e8e0cdceb73e7de56808c05093a5009f
Fliz-none <lhd4388@gmail.com>
2025-09-23 16:46:32 +0700

    Cập nhật footer

--- app/components/layout/Footer.tsx
+++ app/components/layout/Footer.tsx
@@ -3,7 +3,7 @@ import Container from "@/components/ui/Container";
 export default function Footer() {
 	return (
 		<footer className="bg-white border-t">
-			<Container className="py-4 text-center text-gray-500 text-sm">© 2025 MySite. Footer mới sau khi test CI/CD.</Container>
+			<Container className="py-4 text-center text-gray-500 text-sm">Cập nhật footer.</Container>
 		</footer>
 	);
 }
